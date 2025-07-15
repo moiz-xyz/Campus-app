9b456e2f2510a2f7ecae91a36bea6cd25913b9c8
moiz-xyz <a.moizniaxi123@gmail.com>
2025-07-15 04:36:15 -0700

    Your message here

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,27 +1,15 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
-import { db , auth } from "./firebaseConfig"; 
-
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } 
-from "firebase/auth";
+import { auth, db } from "./firebaseConfig";
 
 export const registerUser = async (email, password, name, role) => {
   const userCredential = await createUserWithEmailAndPassword(auth, email, password);
   const user = userCredential.user;
 
-  await setDoc(doc(db, "registeredUsers", user.uid), {
+  await setDoc(doc(db, "users", user.uid), {
     name,
     email,
     role,
-    createdAt: new Date()
+    createdAt: new Date().toISOString(),
   });
-
-  return user;
-};
-
-export const loginUser = (email, password) => {
-  return signInWithEmailAndPassword(auth, email, password);
-};
-
-export const logoutUser = () => {
-  return signOut(auth);
 };
