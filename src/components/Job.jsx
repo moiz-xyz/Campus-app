5e9de66fa3b1ca1edc7c0d5ccc06dbee761b01ea
moiz-xyz <a.moizniaxi123@gmail.com>
2025-08-25 10:16:46 +0500

    added changes according to doc

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import { Button } from './ui/button'
-import { Bookmark } from 'lucide-react'
 import { Avatar, AvatarImage } from './ui/avatar'
 import { Badge } from './ui/badge'
 import { useNavigate } from 'react-router-dom'
+import { Timestamp } from "firebase/firestore";
 
 const Job = ({job}) => {
     const navigate = useNavigate();
 
-    const daysAgoFunction = (mongodbTime) => {
-        const createdAt = new Date(mongodbTime);
-        const currentTime = new Date();
-        const timeDifference = currentTime - createdAt;
-        return Math.floor(timeDifference/(1000*24*60*60));
-    }
+ const daysAgoFunction = (firebaseTime) => {
+  if (!firebaseTime) return null;
+
+  const createdAt = firebaseTime instanceof Timestamp ? firebaseTime.toDate() : new Date(firebaseTime);
+  const currentTime = new Date();
+  const timeDifference = currentTime - createdAt;
+
+  // Convert ms → days
+  return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+};
     
     return (
         <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
             <div className='flex items-center justify-between'>
                 <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) === 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
-                <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button>
             </div>
 
             <div className='flex items-center gap-2 my-2'>
@@ -30,7 +33,7 @@ const Job = ({job}) => {
                 </Button>
                 <div>
                     <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
-                    <p className='text-sm text-gray-500'>India</p>
+                    <p className='text-sm text-gray-500'>Pakistan</p>
                 </div>
             </div>
 
@@ -45,7 +48,6 @@ const Job = ({job}) => {
             </div>
             <div className='flex items-center gap-4 mt-4'>
                 <Button onClick={()=> navigate(`/description/${job?._id}`)} variant="outline">Details</Button>
-                <Button className="bg-[#7209b7]">Save For Later</Button>
             </div>
         </div>
     )
